d9cc198c85f54564c137b5578ee580831dbceeae
Jay Stockhausen <jay.stockhausen@gmail.com>
2024-01-28 20:28:12 +0000

    spring init

---
+++ src/main/java/com/example/springbootjpapostgres/SpringBootJpaPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjpapostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootjpapostgres/SpringBootJpaPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjpapostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
